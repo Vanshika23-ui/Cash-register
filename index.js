5d2f81df14162e0e09666950ea2232389b159f6d
Vanshika <2019vanshika.bajaj@ves.ac.in>
2021-09-27 17:55:35 +0530

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given")
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorforAll(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000,500,100,20,10,5,1];
 
